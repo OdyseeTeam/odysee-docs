c04b4e84baf845b3cae4d2c966c242daa33634dd
Abwasserrohr <abwasserrohr@skyroad.me>
2022-08-12 01:36:03 +0200

    2 files modified, 2 added

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Credits from '@site/src/components/Credits';
+import BrowserWindow from '@site/src/components/BrowserWindow';
 
 export default {
   ...MDXComponents,
   Credits: Credits,
+  BrowserWindow: BrowserWindow,
 };
