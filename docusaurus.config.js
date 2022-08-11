b3db9bebfeca51cdbce7a2cdc38dea0ddccd90f7
Abwasserrohr <abwasserrohr@skyroad.me>
2022-08-11 17:51:26 +0200

    Moved themeConfig up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,88 +1,19 @@
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Odysee Help Hub',
-  tagline: '',
-  url: 'https://docs.odysee.tv',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon-spaceman.png',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  // organizationName: '', // Usually your GitHub org/user name.
-  // projectName: '', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ["en", "es", "fr", "de", "it", "pt"],
-  },
-  plugins: [
-    [
-      require.resolve("@cmfcmf/docusaurus-search-local"),
-      {
-        // whether to index docs pages
-        indexDocs: true,
-
-        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
-        // 0 disables this feature.
-        // 1 indexes the direct parent category in the sidebar of a doc page
-        // 2 indexes up to two nested parent categories of a doc page
-        // 3...
-        //
-        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
-        indexDocSidebarParentCategories: 0,
-
-        // whether to index blog pages
-        indexBlog: false,
-
-        // whether to index static pages
-        // /404.html is never indexed
-        indexPages: false,
-
-        // language of your documentation, see next section
-        language: ["en", "es", "fr", "de", "it", "pt"],
-      },
-    ],
-  ],
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/', // Serve the docs at the site's root
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/OdyseeTeam/odysee-docs/blob/main/',
-          editLocalizedFiles: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      announcementBar: {
+      /*announcementBar: {
         id: 'support_us',
         content:
           'Hey there!',
         backgroundColor: '#fafbfc',
         textColor: '#091E42',
         isCloseable: false,
-      },
+      },*/
       navbar: {
         title: '',
         logo: {
@@ -150,6 +81,73 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  title: 'Odysee Help Hub',
+  tagline: '',
+  url: 'https://docs.odysee.tv',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon-spaceman.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  // organizationName: '', // Usually your GitHub org/user name.
+  // projectName: '', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'fr', 'de', 'it', 'pt'],
+  },
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'),
+      {
+        // whether to index docs pages
+        indexDocs: true,
+
+        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
+        // 0 disables this feature.
+        // 1 indexes the direct parent category in the sidebar of a doc page
+        // 2 indexes up to two nested parent categories of a doc page
+        // 3...
+        //
+        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
+        indexDocSidebarParentCategories: 0,
+
+        // whether to index blog pages
+        indexBlog: false,
+
+        // whether to index static pages
+        // /404.html is never indexed
+        indexPages: false,
+
+        // language of your documentation, see next section
+        language: ['en', 'es', 'fr', 'de', 'it', 'pt'],
+      },
+    ],
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/OdyseeTeam/odysee-docs/blob/main/',
+          editLocalizedFiles: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
 };
 
 module.exports = config;
