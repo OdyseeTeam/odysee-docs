0a425e280892de2b583554f16c33220324ca2357
Abwasserrohr <abwasserrohr@skyroad.me>
2022-08-12 03:20:39 +0200

    Theme toggling ideal images

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -3,10 +3,11 @@ import MDXComponents from '@theme-original/MDXComponents';
 import Credits from '@site/src/components/Credits';
 import BrowserWindow from '@site/src/components/BrowserWindow';
 import ThemedImage from '@theme/ThemedImage';
+import Image from '@theme/IdealImage';
 
 export default {
   ...MDXComponents,
   Credits: Credits,
   BrowserWindow: BrowserWindow,
-  ThemedImage: ThemedImage,
+  Image: Image,
 };
