463ad83f18a3a46bd01b58b62dead27a5b1cce74
Abwasserrohr <abwasserrohr@skyroad.me>
2022-08-12 02:07:50 +0200

    Added themed images

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Credits from '@site/src/components/Credits';
 import BrowserWindow from '@site/src/components/BrowserWindow';
+import ThemedImage from '@theme/ThemedImage';
 
 export default {
   ...MDXComponents,
   Credits: Credits,
   BrowserWindow: BrowserWindow,
+  ThemedImage: ThemedImage,
 };
