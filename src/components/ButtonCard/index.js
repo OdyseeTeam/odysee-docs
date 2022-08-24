4013447b19f11b9a0179fae24794961d8cf1886b
Abwasserrohr <abwasserrohr@skyroad.me>
2022-08-24 20:31:50 +0200

    1 file modified, 2 added, 1 deleted

---
+++ src/components/ButtonCard/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import styles from './styles.module.css';
+import clsx from 'clsx';
+
+function CardContainer({ href, children }) {
+  return (
+    <Link
+      href={href}
+      className={clsx('card padding--lg', styles.cardContainer)}
+    >
+      {children}
+    </Link>
+  );
+}
+function CardLayout({ href, icon, title, description }) {
+  return (
+    <CardContainer href={href}>
+      <h2 className={clsx('text--truncate', styles.cardTitle)} title={title}>
+        {icon} {title}
+      </h2>
+      {description && (
+        <p
+          className={clsx('text--truncate', styles.cardDescription)}
+          title={description}
+        >
+          {description}
+        </p>
+      )}
+    </CardContainer>
+  );
+}
+
+export default function ButtonCard({
+  href,
+  children,
+  color,
+  icon,
+  title,
+  description,
+  to,
+  layout = 'vertical',
+}) {
+  return (
+    <CardContainer href={href}>
+    <h2 className={clsx('text--truncate', styles.cardTitle)} title={title}>
+      {icon} {title}
+    </h2>
+    {description && (
+      <p
+        className={clsx('text--truncate', styles.cardDescription)}
+        title={description}
+      >
+        {description}
+      </p>
+    )}
+  </CardContainer>
+  );
+}
