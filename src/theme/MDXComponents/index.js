1901d8c8d4358115618a5bf50bc71712dd16e05f
Abwasserrohr <abwasserrohr@skyroad.me>
2022-08-25 19:38:38 +0200

    Undo copy & paste error

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
+import Credits from '@site/src/components/Credits';
 import BrowserWindow from '@site/src/components/BrowserWindow';
 import OdyseeButton from '@site/src/components/OdyseeButton';
 import PrimaryOdyseeButton from '@site/src/components/PrimaryOdyseeButton';
@@ -21,6 +22,7 @@ library.add(...iconList);
 
 export default {
   ...MDXComponents,
+  Credits: Credits,
   BrowserWindow: BrowserWindow,
   Image: Image,
   OdyseeButton: OdyseeButton,
