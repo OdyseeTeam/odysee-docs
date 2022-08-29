4728735625949c9e5b6f556311519f73ba71ceec
Abwasserrohr <44294954+Abwasserrohr@users.noreply.github.com>
2022-08-29 15:22:49 +0200

    Added wallet icon

--- src/components/OdyseeIcon.js
+++ src/components/OdyseeIcon.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Credits from '@site/src/components/Credits';
 
-//function getIcon(type) {
-//  return <div>{type == "key" && <span>key.</span>}</div>;
-//}
 function getIcon(type) {
   if (type == 'credits') {
     return Credits();
+  }
+  if (type == 'wallet') {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -4 24 24" overflow="visible" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon--List" aria-hidden="true"><g><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></g></svg>
+    );
   }
   if (type == 'cogwheel') {
     return (
