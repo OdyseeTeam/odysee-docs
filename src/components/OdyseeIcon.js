d3e887a959ade954aaa83413c94e89c9d1105b6f
Abwasserrohr <44294954+Abwasserrohr@users.noreply.github.com>
2022-08-30 01:07:28 +0200

    Added @ sign

--- src/components/OdyseeIcon.js
+++ src/components/OdyseeIcon.js
@@ -5,6 +5,28 @@ function getIcon(type) {
   if (type == 'credits') {
     return Credits();
   }
+  if (type == 'atSign') {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="16"
+        height="16"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon--AtSign"
+        aria-hidden="true"
+      >
+        <g>
+          <circle cx="12" cy="12" r="4"></circle>
+          <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path>
+        </g>
+      </svg>
+    );
+  }
   if (type == 'dollar') {
     return (
       <svg
