adb4779b65c0f5ef3b50bc4b1f3515fab263371a
Abwasserrohr <abwasserrohr@skyroad.me>
2022-09-01 00:59:08 +0200

    Added new cards component to category-basics

--- src/components/ThumbnailCard/index.js
+++ src/components/ThumbnailCard/index.js
@@ -1,47 +1,136 @@
-import React from 'react';
-import Link from '@docusaurus/Link';
-import styles from './styles.module.css';
-import clsx from 'clsx';
-import Image from '@theme/IdealImage';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import React from "react";
+import Link from "@docusaurus/Link";
+import styles from "./styles.module.css";
+import clsx from "clsx";
+import Image from "@theme/IdealImage";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
-function CardContainer({ href, children }) {
+function CardContainer({ href, children, classes }) {
   return (
     <Link
       href={href}
-      className={clsx('card padding--lg', styles.cardContainer)}
+      className={clsx("card padding--lg", styles.cardContainer, classes)}
     >
       {children}
     </Link>
   );
 }
-export default function ThumbnailCard({
-  children,
-  title,
-  description,
-  to,
-  thumbnail,
-  size,
-}) {
+
+function getCard(props) {
+  if (props.thumbnailLocation == "top") {
+    return (
+      <div class={"col col--" + props.size + " margin-bottom--lg"}>
+        <CardContainer href={props.to}>
+          <div>
+            <Link to={props.to}>
+              <Image
+                className={styles.thumbnailCardImage}
+                img={useBaseUrl(props.thumbnail)}
+                alt={`Thumbnail of ${props.title}`}
+              />
+            </Link>
+          </div>
+          <div className={styles.thumbnailCard}>
+            <h3>{props.title}</h3>
+            <p className={styles.cardDescription}>{props.description}</p>
+          </div>
+        </CardContainer>
+      </div>
+    );
+  }
+  if (props.thumbnailLocation == "bottom") {
+    return (
+      <div class={"col col--" + props.size + " margin-bottom--lg"}>
+        <CardContainer href={props.to} classes={styles.rowCardReversedColumn}>
+          <div>
+            <Link to={props.to}>
+              <Image
+                className={styles.thumbnailCardImageReversedColumn}
+                img={useBaseUrl(props.thumbnail)}
+                alt={`Thumbnail of ${props.title}`}
+              />
+            </Link>
+          </div>
+          <div className={styles.thumbnailCard}>
+            <h3>{props.title}</h3>
+            <p className={styles.cardDescription}>{props.description}</p>
+          </div>
+        </CardContainer>
+      </div>
+    );
+  }
+  if (props.thumbnailLocation == "left") {
+    return (
+      <div class={"col col--" + props.size + " margin-bottom--lg"}>
+        <CardContainer href={props.to} classes={styles.rowCard}>
+          <div>
+            <Link to={props.to}>
+              <Image
+                className={styles.thumbnailCardImageRow}
+                img={useBaseUrl(props.thumbnail)}
+                alt={`Thumbnail of ${props.title}`}
+              />
+            </Link>
+          </div>
+          <div className={styles.thumbnailCard}>
+            <h3>{props.title}</h3>
+            <p className={styles.cardDescription}>{props.description}</p>
+          </div>
+        </CardContainer>
+      </div>
+    );
+  }
+  if (props.thumbnailLocation == "right") {
+    return (
+      <div class={"col col--" + props.size + " margin-bottom--lg"}>
+        <CardContainer href={props.to} classes={styles.rowCardReverse}>
+          <div>
+            <Link to={props.to}>
+              <Image
+                className={styles.thumbnailCardImageRow}
+                img={useBaseUrl(props.thumbnail)}
+                alt={`Thumbnail of ${props.title}`}
+              />
+            </Link>
+          </div>
+          <div className={styles.thumbnailCard}>
+            <h3>{props.title}</h3>
+            <p className={styles.cardDescription}>{props.description}</p>
+          </div>
+        </CardContainer>
+      </div>
+    );
+  }
+  if (props.thumbnailLocation == "none") {
+    return (
+      <div class={"col col--" + props.size + " margin-bottom--lg"}>
+        <CardContainer href={props.to}>
+          <div>
+            <Link to={props.to}></Link>
+          </div>
+          <div className={styles.thumbnailCard}>
+            <h3>{props.title}</h3>
+            <p className={styles.cardDescription}>{props.description}</p>
+          </div>
+        </CardContainer>
+      </div>
+    );
+  }
   return (
-    <div class={"col col--" + size + " margin-bottom--lg"}>
-    <CardContainer href={to}>
-      <div>
-        <Link to={to}>
-          <Image
-            className={styles.thumbnailCardImage}
-            img={useBaseUrl(thumbnail)}
-            alt={`Thumbnail of ${title}`}
-          />
-        </Link>
-      </div>
-      <div className={styles.thumbnailCard}>
-        <h3>
-          {children} {title}
-        </h3>
-        <p className={styles.cardDescription}>{description}</p>
-      </div>
-    </CardContainer>
+    <div style={{ width: "100%" }}>
+      ---------
+      <br />
+      manual card: {props.title} - {props.description}
+      <br />
+      <code>
+        title: {props.title}
+        <br />
+        description: {props.description}
+      </code>
     </div>
   );
+}
+
+export default function ThumbnailCard(props) {
+  return getCard(props);
 }
