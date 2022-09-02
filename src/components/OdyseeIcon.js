6f872248f44b025f6ad9f8926ffd1db65e3b804a
Abwasserrohr <abwasserrohr@skyroad.me>
2022-09-02 17:45:24 +0200

    Added screenshots, new translations & more interlinking

--- src/components/OdyseeIcon.js
+++ src/components/OdyseeIcon.js
@@ -6,6 +6,11 @@ function getIcon(inputType) {
   if (type == 'credits') {
     return Credits();
   }
+  if (type == 'menu') {
+	return (
+	  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -4 24 24" overflow="visible" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon--Menu" aria-hidden="true"><g><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></g></svg>
+	);    
+  }
   if (type == 'unlock') {
     return (
       <svg
