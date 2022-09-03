a1c0f1ea87a37f7f06e51a5a4cb2f01068e67023
Abwasserrohr <abwasserrohr@skyroad.me>
2022-09-03 16:37:34 +0200

    Update Docusaurus to 2.1.0

--- src/js/chatBot.js
+++ src/js/chatBot.js
@@ -1,4 +1,4 @@
 // clientModules are still evaluated server-side, this is a workaround:
 if (typeof window !== 'undefined') {
-	!function(){let e=document.createElement("script"),t=document.head||document.getElementsByTagName("head")[0];e.src="https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js",e.async=!0,e.onload=(()=>{window.WebChat.default({customData:{},socketUrl:"http://88.214.58.132:5005"}, null)}),t.insertBefore(e,t.firstChild)}();
+	/*!function(){let e=document.createElement("script"),t=document.head||document.getElementsByTagName("head")[0];e.src="https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js",e.async=!0,e.onload=(()=>{window.WebChat.default({customData:{},socketUrl:"http://88.214.58.132:5005"}, null)}),t.insertBefore(e,t.firstChild)}();*/
 }
